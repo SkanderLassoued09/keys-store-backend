3ea303ab2cb2de65a8ef2380a0a5ec5c5ecaa4ad
Skander LASSOUED <lassoued.skanderr@gmail.com>
2025-09-29 22:44:55 +0100

    add entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ArticleModule } from './article/article.module';
+import { MachineModule } from './machine/machine.module';
+import { EmployeeModule } from './employee/employee.module';
+import { ProviderModule } from './provider/provider.module';
+import { ClientModule } from './client/client.module';
+import { DocumentModule } from './document/document.module';
+import { WorkOrderModule } from './work-order/work-order.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://admin:admin@127.0.0.1:27017/keysstore'),
+    ArticleModule,
+    MachineModule,
+    EmployeeModule,
+    ProviderModule,
+    ClientModule,
+    DocumentModule,
+    WorkOrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
