2a491cb4af55627a1054a4c78d6c594c21d9b125
Skander LASSOUED <lassoued.skanderr@gmail.com>
2025-10-12 15:50:10 +0100

    remove logs for now

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WinstonModule } from 'nest-winston';
-import { createWinstonLogger } from 'logger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger(createWinstonLogger('local')),
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
